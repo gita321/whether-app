9697c03ef201c7d91aa8b20c639b8a0a87230cf4
gita321 <gitanjalikokate921@gmail.com>
2023-10-22 08:12:03 -1200

    adding whether app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './../src/views/Home/Home';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Home/>
+);
